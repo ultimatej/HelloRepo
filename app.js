d278b01576e3018a861c13148d9968fa94d916d2
ultimatej <ultimatej69@gmail.com>
2015-07-22 14:08:48 -0700

    Resuming Dev

---
+++ app.js
@@ -0,0 +1,366 @@
+/*
+* Copyright (c) 2011 Google Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy of
+* the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations under
+* the License.
+*/
+
+// A generic "smart reviver" function.
+// Looks for object values with a `ctor` property and
+// a `data` property. If it finds them, and finds a matching
+// constructor that has a `fromJSON` property on it, it hands
+// off to that `fromJSON` function, passing in the value.
+function Reviver(key, value) {
+  var ctor;
+
+  if (typeof value === "object" &&
+      typeof value.ctor === "string" &&
+      typeof value.data !== "undefined") {
+    ctor = Reviver.constructors[value.ctor] || window[value.ctor];
+    
+    if (typeof ctor === "function" &&
+        typeof ctor.fromJSON === "function") {
+        //console.log("In Reviver. Key: " + key + "value: " + JSON.stringify(value) + " " + ctor);
+        return ctor.fromJSON(value);
+    }
+  }
+  return value;
+}
+Reviver.constructors = {}; // A list of constructors the smart reviver should know about  
+Reviver.constructors.Player = Player;
+Reviver.constructors.playingCards = playingCards;
+Reviver.constructors.playingCards_card = playingCards.card;
+
+// A generic "toJSON" function that creates the data expected
+// by Reviver.
+// `ctorName`  The name of the constructor to use to revive it
+// `obj`       The object being serialized
+// `keys`      (Optional) Array of the properties to serialize,
+//             if not given then all of the objects "own" properties
+//             that don't have function values will be serialized.
+//             (Note: If you list a property in `keys`, it will be serialized
+//             regardless of whether it's an "own" property.)
+// Returns:    The structure (which will then be turned into a string
+//             as part of the JSON.stringify algorithm)
+function Generic_toJSON(ctorName, obj, keys) {
+  var data, index, key;
+
+  if (!keys) {
+    keys = Object.keys(obj); // Only "own" properties are included
+  }
+
+  data = {};
+  for (index = 0; index < keys.length; ++index) {
+    key = keys[index];
+    data[key] = obj[key];
+  }
+  return {ctor: ctorName, data: data};
+}
+
+// A generic "fromJSON" function for use with Reviver: Just calls the
+// constructor function with no arguments, then applies all of the
+// key/value pairs from the raw data to the instance. Only useful for
+// constructors that can be reasonably called without arguments!
+// `ctor`      The constructor to call
+// `data`      The data to apply
+// Returns:    The object
+function Generic_fromJSON(ctor, data) {
+  var obj, name;
+
+  obj = new ctor();
+  for (name in data) {
+    obj[name] = data[name];
+  }
+  return obj;
+}
+    
+function startCountdown(minutes, seconds) {
+    var endTime, msLeft, time;
+    endTime = (new Date().getTime()) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+
+    function updateTimer() {
+        msLeft = endTime - (new Date().getTime());
+        gapi.hangout.data.submitDelta({'countdown': '' + msLeft});
+        if ( msLeft >= 1000 ) {
+            time = new Date( msLeft );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+}
+
+function setCountdown(milliseconds)
+{
+    var element, hours, mins, secs, msLeft, time;
+    element = $('#countdown');
+    
+    msLeft = milliseconds;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    if (msLeft >= 1000) {
+        time = new Date( msLeft );
+        hours = time.getUTCHours();
+        mins = time.getUTCMinutes();
+        secs = time.getUTCSeconds();
+        element.html((hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(secs));
+        
+    }
+    else {
+        //stop the game in all clients, clear canvas, figure out score, post info.
+        element.html("game over!");
+        scoreGame();
+
+    }
+}
+
+function scoreGame() {
+    console.log("game over!");
+    state = gapi.hangout.data.getState();
+    var players = JSON.parse(state['players'], Reviver);
+    var scores = [];
+    for (var i = 0; i < players.length; i++) {
+        players[i].closePositions(state['contractValue']);
+        scores[i] = players[i].getBalance();
+    }
+    var maxScore = Math.max.apply(null, scores);
+    var winner = players[scores.indexOf(maxScore)];
+    winner = players[0];
+    console.log("winner: " + winner);
+    $('#container').empty();
+    $('#container').html("Congratulations winner: " + winner.getName() + "...Score: " + maxScore + 
+        "\n" + players[0].getName() + "...Score: " + scores[0]);
+}
+
+function startButtonClick() {
+    var cardDeck = new playingCards();
+    var updates = {};
+    var players = [];
+    var maxSpread = 4;    
+    var enabledParticipants = gapi.hangout.getEnabledParticipants();
+    var numPlayers = enabledParticipants.length;
+    var expectedValue = numPlayers * 2 * 7;
+    var startingBid = expectedValue - maxSpread / 2;
+    var startingAsk = expectedValue + maxSpread / 2;
+    var minutes = 0;
+    var seconds = 10;
+    var contractValue = 0;
+
+    for (var i = 0; i < numPlayers; i++) {
+        var currParticipant = enabledParticipants[i];
+        console.log("Participant name: " + currParticipant.person.displayName + "Participant id: " + currParticipant.person.id);
+        var currPlayer = new Player(currParticipant.id, currParticipant.person.displayName);
+        currPlayer.dealHand(cardDeck);
+        contractValue += currPlayer.getHandRankSum();
+        currPlayer.setBid(startingBid);
+        currPlayer.setAsk(startingAsk);
+        players[i] = currPlayer;        
+    }
+    var randomPlayerName = enabledParticipants[Math.floor(Math.random() * numPlayers)].person.displayName;
+    updates['players'] = JSON.stringify(players);
+    updates['cardDeck'] = JSON.stringify(cardDeck);
+    updates['expectedValue'] = '' + expectedValue;
+    updates['maxSpread'] = '' + maxSpread;
+    updates['marketAsk'] = '' + startingAsk;
+    updates['marketBid'] = '' + startingBid;
+    updates['marketAskPlayer'] = '' + randomPlayerName;
+    updates['marketBidPlayer'] = '' + randomPlayerName;
+    updates['contractValue'] = '' + contractValue;
+    updates['countdown'] = '' + 1000 * (minutes * 60 + seconds);
+    gapi.hangout.data.submitDelta(updates);
+    startCountdown(minutes, seconds );
+}
+
+// This button will increment the value
+function plusButtonClick() {
+   // Stop acting like a button
+    //e.preventDefault();
+    //var updates = {};
+    var state = gapi.hangout.data.getState();
+    var players = JSON.parse(state['players'], Reviver);
+    var player = getPlayerById(players, getUserHangoutId());
+    player.incrementBidAsk();
+    //updates['players'] = JSON.stringify(players);
+    //gapi.hangout.data.submitDelta(updates);
+    updateMarket(players);
+}
+// This button will decrement the value till 0
+function minusButtonClick() {
+    // Stop acting like a button
+    //e.preventDefault();
+    //var updates = {};
+    var state = gapi.hangout.data.getState();
+    var players = JSON.parse(state['players'], Reviver);
+    var player = getPlayerById(players, getUserHangoutId());
+    player.decrementBidAsk();
+    //updates['players'] = JSON.stringify(players);
+    //gapi.hangout.data.submitDelta(updates);
+    updateMarket(players);
+}
+
+function buyButtonClick() {
+    // Stop acting like a button
+    //e.preventDefault();
+    var state = gapi.hangout.data.getState();
+    var players = JSON.parse(state['players'], Reviver);
+    var player = getPlayerById(players, getUserHangoutId());
+    var marketAsk = parseInt(state['marketAsk']);
+    var marketAskPlayer = state['marketAskPlayer'];
+    var maxSpread = parseInt(state['maxSpread']);
+    if (player.getName() === marketAskPlayer) {
+        console.log("Can't buy from yourself, increase bid/ask");
+        //player.setAsk(undefined);
+        //updateMarket(players);
+    }
+    else {
+        player.setBid(marketAsk);
+        player.setAsk(marketAsk + maxSpread);
+        player.buy(marketAsk);
+        getPlayerByName(players, marketAskPlayer).sell(marketAsk);
+        updateMarket(players);
+    }
+}
+
+function sellButtonClick() {
+    // Stop acting like a button
+    //e.preventDefault();
+    var state = gapi.hangout.data.getState();
+    var players = JSON.parse(state['players'], Reviver);
+    var player = getPlayerById(players, getUserHangoutId());
+    var marketBid = parseInt(state['marketBid']);
+    var marketBidPlayer = state['marketBidPlayer'];
+    var maxSpread = parseInt(state['maxSpread']);
+    if (player.getName() === marketBidPlayer) {
+        console.log("Can't sell to yourself, decrease bid/ask");
+        //player.setAsk(undefined);
+        //updateMarket(players);
+    }
+    else {
+        player.setAsk(marketBid);
+        player.setBid(marketBid - maxSpread);
+        player.sell(marketBid);
+        getPlayerByName(players, marketBidPlayer).buy(marketBid);
+        updateMarket(players);
+    }
+}
+
+function updateMarket(players) {
+    var updates = {};
+    //var state = gapi.hangout.data.getState();
+    //console.log(state);
+    //var players = JSON.parse(state['players'], Reviver);
+    console.log("updateMarket_players: " + JSON.stringify(players))
+    var bids = [];
+    var asks = [];
+    for (var i = 0; i < players.length; i++) {
+        bids[i] = players[i].getBid();
+        asks[i] = players[i].getAsk();
+    }
+    var minAsk = Math.min.apply(null, asks);
+    var minAskPlayer = players[asks.indexOf(minAsk)];
+    var maxBid = Math.max.apply(null, bids);
+    console.log("maxBid: " + maxBid);
+    var maxBidPlayer = players[bids.indexOf(maxBid)];
+    console.log("maxBidPlayer: " + JSON.stringify(maxBidPlayer));
+    updates['marketAsk'] = '' + minAsk;
+    updates['marketAskPlayer'] = '' + minAskPlayer.getName();
+    updates['marketBid'] = '' + maxBid;
+    updates['marketBidPlayer'] = '' + maxBidPlayer.getName();
+    updates['players'] = JSON.stringify(players);
+    gapi.hangout.data.submitDelta(updates);
+}
+
+function getPlayerById(players, id) {
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].getId() === id)
+            return players[i];
+    }
+    return undefined;
+}
+
+function getPlayerByName(players, name) {
+    for (var i = 0; i < players.length; i++) {
+        //console.log("Player Name: " + players[i].getName() + "name" + name);
+        if (players[i].getName() === name)
+            //console.log("matches: " + name);
+            return players[i];
+    }
+    return undefined;
+}
+
+function getUserHangoutId() {
+  return gapi.hangout.getLocalParticipantId();
+}
+
+
+var forbiddenCharacters = /[^a-zA-Z!0-9_\- ]/;
+function setText(element, text) {
+  element.innerHTML = typeof text === 'string' ?
+      text.replace(forbiddenCharacters, '') :
+      '';
+}
+
+function updateStateUi(state) {
+    console.log(state);
+    var players = JSON.parse(state['players'], Reviver);
+    var player = getPlayerById(players, getUserHangoutId());
+    //var cardDeck = JSON.parse(state['cardDeck'], Reviver);
+    //console.log("player: " + JSON.stringify(player));
+    //console.log("cardDeck: " + JSON.stringify(cardDeck));
+
+    //console.log("work please: " + player.getName() + "playerID: " + player.getId());
+    $('#startButton').hide();
+    player.updateUI();
+    $('#marketBid').html("Market Bid: " + state['marketBid']);
+    $('#marketBidPlayer').html("(" + state['marketBidPlayer'] + ")");
+    $('#marketAsk').html("Market Ask: " + state['marketAsk']);
+    $('#marketAskPlayer').html("(" + state['marketAskPlayer'] + ")");
+    $('#expectedValue').html("(Expected: " + state['expectedValue'] + ")");
+    setCountdown(parseInt(state['countdown']));
+}
+
+function updateParticipantsUi(participants) {
+    console.log('Participants count: ' + participants.length);
+    var participantsListElement = document.getElementById('participants');
+    setText(participantsListElement, participants.length.toString());
+}
+
+// A function to be run at app initialization time which registers our callbacks
+function init() {
+  console.log('Init app.');
+
+  var apiReady = function(eventObj) {
+    if (eventObj.isApiReady) {
+        console.log('API is ready');
+
+        gapi.hangout.data.onStateChanged.add(function(eventObj) {
+            updateStateUi(eventObj.state);
+        });
+      //  gapi.hangout.onParticipantsChanged.add(function(eventObj) {
+      //  updateParticipantsUi(eventObj.participants);
+      // });
+      // updateStateUi(gapi.hangout.data.getState());
+      //  updateParticipantsUi(gapi.hangout.getParticipants());
+        
+        gapi.hangout.onApiReady.remove(apiReady);
+    }
+  };
+
+  // This application is pretty simple, but use this special api ready state
+  // event if you would like to any more complex app setup.
+  gapi.hangout.onApiReady.add(apiReady);
+}
+
+gadgets.util.registerOnLoadHandler(init);
---
+++ player.js
@@ -0,0 +1,120 @@
+function Player(id, name) {
+	this.id = id;
+  this.name = name;
+	this.hand = [];
+	this.handRankSum = undefined;
+	this.bid = undefined;
+	this.ask = undefined;
+	this.currPosition = 0;
+	this.currBalance = 0; 
+}
+
+Player.prototype.getId = function() {
+	return this.id;
+}
+
+Player.prototype.getName = function() {
+	return this.name;
+}
+
+Player.prototype.getHandRankSum = function() {
+  return this.handRankSum;
+}
+
+Player.prototype.getBalance = function() {
+  return this.currBalance;
+}
+
+Player.prototype.dealHand = function(cardDeck) {
+    for (var i = 0; i < 2; i++) {
+        var c = cardDeck.draw();
+        if(!c) {
+        console.log('no more cards');
+        return;
+    }
+        this.hand[this.hand.length] = c;  
+    }
+    this.calculateHandRankSum();
+}
+
+Player.prototype.updateUI = function() {
+    this.showHand();
+    $('#yourBid').val(this.bid);
+    $('#yourAsk').val(this.ask);
+    $('#handRankSum').html(this.handRankSum);
+    $('#currentPosition').html("Position: " + this.currPosition + 
+      "...Cost Basis: " + (this.currPosition ? this.currBalance / 
+      Math.abs(this.currPosition) : this.currBalance));  
+}
+
+Player.prototype.showHand = function() {
+//function showHand(hand) {
+    var card1 = $('#cardOne')
+    var card2 = $('#cardTwo')
+    card1.html('');
+    card2.html('');
+    card1.append(this.hand[0].getHTML());
+    card2.append(this.hand[1].getHTML());
+}
+
+Player.prototype.calculateHandRankSum = function() {
+//function calculateHandRankSum(hand) {
+    var handRankSum = 0;
+    for (var i = 0; i < this.hand.length; i++) {
+        handRankSum += this.hand[i].numericRank();
+    }
+    console.log(handRankSum);
+    this.handRankSum = handRankSum;
+}
+
+Player.prototype.getBid = function() {
+	return this.bid;
+}
+
+Player.prototype.getAsk = function() {
+	return this.ask;
+}
+
+Player.prototype.setBid = function(bid) {
+	this.bid = bid;
+}
+
+Player.prototype.setAsk = function(ask) {
+	this.ask = ask;
+}
+
+Player.prototype.decrementBidAsk = function() {
+  console.log('Update Bid/Ask clicked.');
+  this.setBid(this.bid - 1);
+  this.setAsk(this.ask - 1);
+  console.log('New bid ' + this.getBid() + '...New ask ' + this.getAsk());
+}
+
+Player.prototype.incrementBidAsk = function() {
+  console.log('Update Bid/Ask clicked.');
+  this.setBid(this.bid + 1);
+  this.setAsk(this.ask + 1);
+  console.log('New bid ' + this.getBid() + '...New ask ' + this.getAsk());
+}
+
+Player.prototype.buy = function(price) {
+	this.currPosition++;
+	this.currBalance -= price;
+}
+
+Player.prototype.sell = function(price) {
+	this.currPosition--;
+	this.currBalance += price;
+}
+
+Player.prototype.closePositions = function(positionValue) {
+  this.currBalance += this.currPosition * positionValue;
+  this.currPosition = 0;
+}
+
+Player.prototype.toJSON = function() {
+    return Generic_toJSON("Player", this);
+};
+Player.fromJSON = function(value) {
+    return Generic_fromJSON(Player, value.data);
+};
